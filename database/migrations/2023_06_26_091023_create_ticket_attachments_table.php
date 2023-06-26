3421fbb62acfe4ef2a230ba4b642ad4c27cf29f6
Jody-septiawan <jody.septiawan5@gmail.com>
2023-06-26 17:37:16 +0800

    feature: database setup

---
+++ database/migrations/2023_06_26_091023_create_ticket_attachments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket_attacments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('ticket_id');
+            $table->text('url');
+            $table->string('filename', 50);
+            $table->decimal('size', 10, 2);
+            $table->string('type', 50);
+            $table->timestamps();
+
+            $table->foreign('ticket_id')->references('id')->on('tickets')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ticket_attacments');
+    }
+}
